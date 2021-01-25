6d3294784fd2da98cf72c9e9341283cef02e7ce1
Muhammad Nouman <noumanbhai006@gmail.com>
2021-01-25 17:46:15 +0500

    Admin Panel integration complete
    Admin Panel integration complete

---
+++ resources/views/admin/layout/footer.blade.php
@@ -0,0 +1,9 @@
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto text-primary">
+			<span><b>Copyright &copy; Your Website 2021</b></span>
+			<br>
+			<span><b>Create by; &#8594;Muhammad Nouman</b></span>
+		</div>
+	</div>
+</footer>
--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,7 @@ Route::get('/', function () {
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+
+    return view('admin.index');
 
 })->name('dashboard');
