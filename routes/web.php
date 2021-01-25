3be42838cda2e0a81c3b643ab70eac9adf59d254
Muhammad Nouman <noumanbhai006@gmail.com>
2021-01-25 15:20:21 +0500

    Start
    Start

--- routes/web.php
+++ routes/web.php
@@ -19,8 +19,5 @@ Route::get('/', function () {
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
-    pak
 
-
-    jjj
 })->name('dashboard');
