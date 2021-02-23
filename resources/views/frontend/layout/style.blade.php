80502dd1d1528c41f733e2883751c35ce2bd5743
Muhammad Nouman <noumanbhai006@gmail.com>
2021-02-23 11:47:47 -0500

    frontend integration
    frontend integration

---
+++ resources/views/frontend/layout/style.blade.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" type="text/css" href="{{asset('frontend/assets/styles/bootstrap4/bootstrap.min.css') }}">
+<link href="{{asset('frontend/assets/plugins/fontawesome-free-5.0.1/css/fontawesome-all.css')}}" rel="stylesheet" type="text/css">
+<link rel="stylesheet" type="text/css" href="{{asset('frontend/assets/plugins/OwlCarousel2-2.2.1/owl.carousel.css')}}">
+<link rel="stylesheet" type="text/css" href="{{asset('frontend/assets/plugins/OwlCarousel2-2.2.1/owl.theme.default.css') }}">
+<link rel="stylesheet" type="text/css" href="{{asset('frontend/assets/plugins/OwlCarousel2-2.2.1/animate.css') }}">
+<link rel="stylesheet" type="text/css" href="{{asset('frontend/assets/plugins/slick-1.8.0/slick.css') }}">
+<link rel="stylesheet" type="text/css" href="{{asset('frontend/assets/styles/main_styles.css') }}">
+<link rel="stylesheet" type="text/css" href="{{asset('frontend/assets/styles/responsive.css') }}">
