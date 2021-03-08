5b358bcae9feb127c0c8bbe9ce09d8b0eaad1d26
Muhammad Nouman <noumanbhai006@gmail.com>
2021-03-08 01:31:29 -0500

    Mid slider complete but range 4 is not include
    Mid slider complete but range 4 is not include

--- resources/views/frontend/main.blade.php
+++ resources/views/frontend/main.blade.php
@@ -355,87 +355,45 @@ font-weight: 500;" >Rs{{$product->selling_price}}</span></div>
 		<!-- Banner -->
 
 		<div class="banner_2">
-			<div class="banner_2_background" style="background-image:url(images/banner_2_background.jpg')}}')"></div>
+<div class="banner_2_background" style="background-image:url('{{ asset('frontend/assets/images/banner_2_background.jpg')}}')"></div>
 			<div class="banner_2_container">
 				<div class="banner_2_dots"></div>
 				<!-- Banner 2 Slider -->
 
 				<div class="owl-carousel owl-theme banner_2_slider">
 
-					<!-- Banner 2 Slider Item -->
-					<div class="owl-item">
-						<div class="banner_2_item">
-							<div class="container fill_height">
-								<div class="row fill_height">
-									<div class="col-lg-4 col-md-6 fill_height">
-										<div class="banner_2_content">
-											<div class="banner_2_category">Laptops</div>
-											<div class="banner_2_title">MacBook Air 13</div>
-											<div class="banner_2_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum laoreet.</div>
-											<div class="rating_r rating_r_4 banner_2_rating"><i></i><i></i><i></i><i></i><i></i></div>
-											<div class="button banner_2_button"><a href="#">Explore</a></div>
-										</div>
+<!-- Banner 2 Slider Item -->
+@foreach($products as $product)
+@if($product->mid_slider==1)
 
-									</div>
-									<div class="col-lg-8 col-md-6 fill_height">
-										<div class="banner_2_image_container">
-											<div class="banner_2_image"><img src="{{asset('frontend/assets/images/banner_2_product.png')}}" alt=""></div>
-										</div>
-									</div>
-								</div>
-							</div>			
-						</div>
-					</div>
+<div class="owl-item">
+<div class="banner_2_item">
+<div class="container fill_height">
+<div class="row fill_height">
+<div class="col-lg-4 col-md-6 fill_height">
+<div class="banner_2_content">
+<div class="banner_2_category">{{ $product->allcats->category_name }}</div>
+<div class="banner_2_title">{{ $product->product_name }}</div>
+<div class="banner_2_text">{{ $product->allbrands->brand_name }}
+<br>
+{{ $product->selling_price }}
+</div>
+<div class="rating_r rating_r_4 banner_2_rating"><i></i><i></i><i></i><i></i><i></i></div>
+<div class="button banner_2_button"><a href="#">Explore</a></div>
+</div>
 
-					<!-- Banner 2 Slider Item -->
-					<div class="owl-item">
-						<div class="banner_2_item">
-							<div class="container fill_height">
-								<div class="row fill_height">
-									<div class="col-lg-4 col-md-6 fill_height">
-										<div class="banner_2_content">
-											<div class="banner_2_category">Laptops</div>
-											<div class="banner_2_title">MacBook Air 13</div>
-											<div class="banner_2_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum laoreet.</div>
-											<div class="rating_r rating_r_4 banner_2_rating"><i></i><i></i><i></i><i></i><i></i></div>
-											<div class="button banner_2_button"><a href="#">Explore</a></div>
-										</div>
-
-									</div>
-									<div class="col-lg-8 col-md-6 fill_height">
-										<div class="banner_2_image_container">
-											<div class="banner_2_image"><img src="{{asset('frontend/assets/images/banner_2_product.png')}}" alt=""></div>
-										</div>
-									</div>
-								</div>
-							</div>			
-						</div>
-					</div>
-
-					<!-- Banner 2 Slider Item -->
-					<div class="owl-item">
-						<div class="banner_2_item">
-							<div class="container fill_height">
-								<div class="row fill_height">
-									<div class="col-lg-4 col-md-6 fill_height">
-										<div class="banner_2_content">
-											<div class="banner_2_category">Laptops</div>
-											<div class="banner_2_title">MacBook Air 13</div>
-											<div class="banner_2_text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas fermentum laoreet.</div>
-											<div class="rating_r rating_r_4 banner_2_rating"><i></i><i></i><i></i><i></i><i></i></div>
-											<div class="button banner_2_button"><a href="#">Explore</a></div>
-										</div>
-
-									</div>
-									<div class="col-lg-8 col-md-6 fill_height">
-										<div class="banner_2_image_container">
-											<div class="banner_2_image"><img src="{{asset('frontend/assets/images/banner_2_product.png')}}" alt=""></div>
-										</div>
-									</div>
-								</div>
-							</div>			
-						</div>
-					</div>
+</div>
+<div class="col-lg-8 col-md-6 fill_height">
+<div class="banner_2_image_container">
+<div class="banner_2_image" ><img  style="width: 70%"  src="{{ URL::to('/') }}/media/product/{{ $product->image_one }}" alt=""></div>
+</div>
+</div>
+</div>
+</div>			
+</div>
+</div>
+@endif
+@endforeach
 
 				</div>
 			</div>
