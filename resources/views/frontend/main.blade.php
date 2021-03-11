9b964f026a350609d97bf1198c2004d8958bdb61
Muhammad Nouman <noumanbhai006@gmail.com>
2021-03-11 13:05:16 -0500

    category one not complete
    category one not complete

--- resources/views/frontend/main.blade.php
+++ resources/views/frontend/main.blade.php
@@ -398,8 +398,62 @@
 	</div>
 </div>
 
-<!-- Hot New Arrivals -->
+<!-- Hot New One -->
 
+<div class="new_arrivals">
+	<div class="container">
+		<div class="row">
+			<div class="col">
+				<div class="tabbed_container">
+					<div class="tabs clearfix tabs-right">
+			<div class="new_arrivals_title">Category One Product</div>
+						<ul class="clearfix">
+							<li class="active"></li>
+							<li></li>
+							<li></li>
+						</ul>
+						<div class="tabs_line"><span></span></div>
+					</div>
+					<div class="row">
+						<div class="col-lg-9" style="z-index:1;">
+
+							<!-- Product Panel -->
+							<div class="product_panel panel active">
+								<div class="arrivals_slider slider">
+
+									<!-- Slider Item -->
+									<div class="arrivals_slider_item">
+										<div class="border_active"></div>
+										<div class="product_item is_new d-flex flex-column align-items-center justify-content-center text-center">
+											<div class="product_image d-flex flex-column align-items-center justify-content-center"><img src="{{asset('frontend/assets/images/new_1.jpg')}}" alt=""></div>
+											<div class="product_content">
+												<div class="product_price">$225</div>
+												<div class="product_name"><div><a href="product.html">Astro M2 Black</a></div></div>
+												<div class="product_extras">
+													<div class="product_color">
+														<input type="radio" checked name="product_color" style="background:#b19c83">
+														<input type="radio" name="product_color" style="background:#000000">
+														<input type="radio" name="product_color" style="background:#999999">
+													</div>
+													<button class="product_cart_button">Add to Cart</button>
+												</div>
+											</div>
+											<div class="product_fav"><i class="fas fa-heart"></i></div>
+											<ul class="product_marks">
+												<li class="product_mark product_discount">-25%</li>
+												<li class="product_mark product_new">new</li>
+											</ul>
+										</div>
+									</div>
+								</div>
+								<div class="arrivals_slider_dots_cover"></div>
+							</div>
+						</div></div></div>
+					</div>
+				</div>
+			</div>		
+		</div>
+<!--Hot New  Multiple  -->
 <div class="new_arrivals">
 	<div class="container">
 		<div class="row">
@@ -1668,7 +1722,6 @@
 		</div>
 	</div>		
 </div>
-
 <!-- Best Sellers -->
 
 <div class="best_sellers">
